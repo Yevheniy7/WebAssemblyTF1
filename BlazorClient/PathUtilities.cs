f0889c745e7e7795de0d632b5b65d0d293ad34cb
yevhe <yevhe@LAPTOP-35UFDHVQ>
2021-06-23 12:02:53 +0200

    Add project files.

---
+++ BlazorClient/PathUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace BlazorServer
+{
+    public class PathUtilities
+    {
+        public static string GetPathFromBinFolder(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+}
